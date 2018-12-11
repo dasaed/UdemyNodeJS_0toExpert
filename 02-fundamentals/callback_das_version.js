a045af4a1f991ef0b9f52938dc058d35dcf26cbe
Dasaed <carlos.dasaed@gmail.com>
2018-12-10 22:48:21 -0330

    understanding callbacks

--- 02-fundamentals/callback_das_version.js
+++ 02-fundamentals/callback_das_version.js
@@ -4,28 +4,58 @@ In javascript, you can input functions into functions.
 
 let add = (a, b) => {
     return a + b;
-}
+};
 
 let sub = (a, b) => {
     return a - b;
-}
+};
 
 let div = (a, b) => {
     return a / b;
-}
+};
 
-let mul = (a, b) => {
+let mul = function(a, b) {
     return a * b;
-}
+};
 
 let mod = (a, b) => {
     return a % b;
-}
+};
 
-let myFun = (num1, num2, operation) {
-    if (typeof operation === "function") {
-        operation(num1, num2);
+let calc = function(num1, num2, operation) {
+    return operation(num1, num2);
+};
+
+console.log("This is the value  of the calcuation is: " + calc(10, 6, mod));
+
+var myArray = [{
+        number: 34,
+        name: 'Erika'
+    },
+    {
+        number: 15,
+        name: 'Joshua'
+    },
+    {
+        number: 12,
+        name: 'Elkin'
+    },
+    {
+        number: 94,
+        name: 'Omega'
+    },
+    {
+        number: 1,
+        name: 'Alpha'
     }
-}
 
-console.log(myFun(8, 10, add));
+];
+
+myArray.sort(function(val1, val2) {
+    if (val1.number < val2.number) {
+        return -1;
+    } else {
+        return 1;
+    }
+});
+console.log(myArray)
